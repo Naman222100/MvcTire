cb53f4b4b17684667030d90d412fa8cf012f5969
W0822254 <W0822254@ADL-A1201I-10>
2023-10-13 13:15:28 -0400

    I ran command for nugget package in pmc

---
+++ Models/Tire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTire.Models
+{
+    public class Tire
+    {
+            public int Id { get; set; }
+            public string Brand { get; set; }
+            public string Colour { get; set; }
+            public decimal Price { get; set; }
+            public decimal Size { get; set; }
+
+
+    }
+}
