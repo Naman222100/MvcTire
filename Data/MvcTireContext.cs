4708b2eb93c40375e3e3c7e65816308e79c368c9
W0822254 <W0822254@ADL-A1201I-10>
2023-10-13 13:40:30 -0400

    I created a new folder called Data and added a file named MvcTireContext

---
+++ Data/MvcTireContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTire.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tire> Tire { get; set; }
+    }
+}
